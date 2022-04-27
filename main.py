f3822c0353627eabea09692a3f6cff3e76f32cd5
RationalNivesh <104433699+RationalNivesh@users.noreply.github.com>
2022-04-27 15:45:44 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#Input
+num1=int(input("What is your number ?"))
+num2=int(input("What is your number ?"))
+num3=int(input("What is your number ?"))
+#Math
+total=num1+num2+num3
+mean=total/3
+print(mean)
